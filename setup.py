b8b9c941cbaab66ff70d40c84874693d5a61636f
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2020-02-01 21:33:14 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='pybf',
+    version='0.1.0',
+    description='A Python wrapper for bitFlyer API.',
+    url='https://github.com/tapioka324/pybf',
+    packages=['pybf'],
+    install_requires=['requests']
+)
