6dc1c66722a01a1c9bba8e3d077f27c15982c3d3
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2020-02-10 22:44:58 +0900

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pybf',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python wrapper for bitFlyer API.',
     url='https://github.com/tapioka324/pybf',
     packages=['pybf'],
