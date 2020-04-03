e946ed8c9d16dcbff1a2522c993df851ec090129
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2020-04-04 00:05:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
 
+requires = [
+    'requests',
+    'websocket-client'
+]
+
+
 setup(
     name='pybf',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python wrapper for bitFlyer API.',
     url='https://github.com/tapioka324/pybf',
     packages=['pybf'],
-    install_requires=['requests']
+    install_requires=requires
 )
