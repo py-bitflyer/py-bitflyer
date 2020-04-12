1c33172964b090c205474943a1ae9087dd6fdf94
tapioka324 <26566765+tapioka324@users.noreply.github.com>
2020-04-12 20:12:05 +0900

    :sparkles: Add long_description

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 requires = [
     'requests',
     'websocket-client'
@@ -11,6 +14,8 @@ setup(
     name='py_bitflyer',
     version='0.1.3',
     description='A Python wrapper for bitFlyer API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/py-bitflyer/py-bitflyer',
     license='MIT License',
     packages=['py_bitflyer'],
